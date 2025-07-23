88df37a94b78343fe5bdefb541745078c9aa4c41
tarakram-123 <tarakram75693@gmail.com>
2025-07-23 17:37:18 +0530

    Update vite.config.js

--- leaderboard/frontend/vite.config.js
+++ leaderboard/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://leaderboard-backend-g15h.onrender.com',
         changeOrigin: true,
         secure: false,
       }
